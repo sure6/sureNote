341c27b59d9dfe8c29904bcbaeafca5fe834ce84
leesure <arm125@qq.com>
2021-08-15 13:05:20 +0800

    modify static files

--- sureNote/settings.py
+++ sureNote/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,6 +126,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 
 
 # Default primary key field type
